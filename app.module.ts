5739aaad8cf64dcefad11b792e71334994d80f79
Rosemole5091 <52654525+Rosemole5091@users.noreply.github.com>
2019-07-10 10:45:10 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompComponent } from './comp/comp.component';
+import { SignFormComponent } from './sign-form/sign-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompComponent,
+    SignFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ sign-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'sign-form',
+  templateUrl: './sign-form.component.html',
+  styleUrls: ['./sign-form.component.css']
+})
+export class SignFormComponent {
+
+ log(x) {console.log(x);}
+
+}
+
